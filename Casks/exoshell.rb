b7abb048ddbc5fa5ba35c5b5fb7e2d84c725c358
Charles Tison <ctison@pm.me>
2024-05-29 09:53:12 +0200

    exoshell 0.2.0

--- Casks/exoshell.rb
+++ Casks/exoshell.rb
@@ -1,28 +1,27 @@
 cask "exoshell" do
-  homepage "https://exoshell.io"
-  name "exoshell"
-  desc "Modern terminal"
-  version "0.1.0"
-
   arch arm: "aarch64", intel: "x86_64"
 
-  url "https://github.com/exoshell-dev/exoshell/releases/download/v#{version}/ExoShell_darwin_#{arch}.dmg",
-    verified: "github.com/exoshell-dev/exoshell/"
+  version "0.2.0"
+  sha256 arm:   "6251c049e3749d9ffea09c9963b757db865af4319052f1f702295054b49f7f19",
+         intel: "6718e7665ed7faaa743748c4a89ecb3617038bc121f49f8fa9294acef9c238a4"
 
-  sha256 arm: "2baae5b731b26f8c97d8d0ac702e7799e7427ab7cd1118975ef8ef18dd67d388",
-    intel: "76235eba606d2a55f7bdcb4eb49097ceaab635a0c661431077adf1f6598b64c5"
+  url "https://github.com/exoshell-dev/exoshell/releases/download/v#{version}/ExoShell_darwin_#{arch}.dmg",
+      verified: "github.com/exoshell-dev/exoshell/"
+  name "exoshell"
+  desc "Modern terminal"
+  homepage "https://exoshell.io/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
 
   app "ExoShell.app"
 
   postflight do
     system_command "/usr/bin/xattr",
-      args: ["-rd", "com.apple.quarantine", "#{appdir}/ExoShell.app"]
-  end
-
-  auto_updates true
-
-  livecheck do
-    url :url
-    strategy :github_latest
+                   args: ["-rd", "com.apple.quarantine", "#{appdir}/ExoShell.app"]
   end
 end
